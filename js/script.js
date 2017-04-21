924e16d1ec22fc550fd5699b07b0d25ffd36d043
Mark Ian Pamintuan <ianmayfire@gmail.com>
2017-04-21 16:02:10 +0800

    First commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var shape = document.getElementById("shape");
+var displayTimeElapsed = document.getElementById("time");
+var randomInterval = 0;
+var randomInterval2 = 0;
+var timeElapsed;
+var start;
+var end;
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function spawnBox() {
+    start = new Date().getTime();
+    randomInterval = Math.floor(Math.random() * (500-1)) + 1;
+    randomInterval2 = Math.floor(Math.random() * (1200-1)) + 1;
+    shape.style.display = "block";
+    shape.style.top = randomInterval + "px";
+    shape.style.left = randomInterval2 + "px";
+    shape.style.backgroundColor = getRandomColor();
+}
+
+setTimeout(spawnBox, 1000);
+
+shape.onclick = function() {
+    end = new Date().getTime();
+    timeElapsed = end - start;
+    displayTimeElapsed.innerHTML = "Time elapsed: " + parseFloat(timeElapsed/1000).toFixed(2) + " seconds";
+    shape.style.display = "none";
+    randomInterval = (Math.floor(Math.random() * (3-1)) + 1) * 1000;
+    setTimeout(spawnBox, randomInterval);
+}
